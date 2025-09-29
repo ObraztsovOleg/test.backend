deac70e59d726d1825a62426dac214e72f7ebeae
Obraztsov Oleg Aleksandrovich <olegrasputina@gmail.com>
2025-09-29 12:14:39 +0300

    [*]

---
+++ src/main/java/ufs/pipeline/test/app/backend/HealthData.java
@@ -0,0 +1,9 @@
+package ufs.pipeline.test.app.backend;
+
+import lombok.Data;
+
+@Data
+public class HealthData {
+    private String message;
+    private String status;
+}
---
+++ src/test/java/ufs/pipeline/test/app/backend/HttpRequestTest.java
@@ -0,0 +1,30 @@
+package ufs.pipeline.test.app.backend;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HttpRequestTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	void greetingShouldReturnDefaultMessage() throws Exception {
+		HealthData data = this.restTemplate.getForObject("http://localhost:" + port + "/health/check", HealthData.class);
+
+        assertThat(data.getMessage().contains("Hello"));
+        assertThat(data.getStatus().contains("FAILURE"));
+	}
+}
